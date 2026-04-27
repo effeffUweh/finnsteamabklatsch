53192440619bd563310bb439cc20719cb917a731
finn <ffuw123@gmail.com>
2026-04-27 14:12:37 +0200

    testhallo

--- sheet01/main/LeibnizPi.java
+++ sheet01/main/LeibnizPi.java
@@ -5,7 +5,7 @@ public class LeibnizPi {
         double sum = 0;
         for(int k = 0; k <= n; k++){
             double i = 2.0 * k + 1.0;
-
+            String hallo = "hi";
             if(k % 2 == 1){
                 sum -= 1.0 / i;
             }
