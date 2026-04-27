dea50e39448639db6f5b9615d16453c275176be6
finn <ffuw123@gmail.com>
2026-04-27 14:04:23 +0200

    test

--- sheet01/main/LeibnizPi.java
+++ sheet01/main/LeibnizPi.java
@@ -3,7 +3,7 @@ public class LeibnizPi {
     static void main() {
         int n = 1_000_000;
         double sum = 0;
-
+//test
         for(int k = 0; k <= n; k++){
             double i = 2.0 * k + 1.0;
 
